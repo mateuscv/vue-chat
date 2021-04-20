eafeae621aebe005bec28f5acf6110400ae818d5
mateuscv <mateus@furg.br>
2021-04-20 10:48:17 -0300

    added reactive connection bar

---
+++ src/components/ConnectionStatus.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <span v-if="isConnected" class="bar connected">
+      Connected to the server
+    </span>
+    <span v-else class="bar disconnected">
+      Disconnected from the server.
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConnectionStatus",
+  props: ["isConnected"]
+};
+</script>
+
+<style>
+.bar {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  padding: 5px;
+}
+.connected {
+  background: greenyellow;
+  color: black;
+}
+.disconnected {
+  background: red;
+  color: white;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import SocketIO from "socket.io-client";
 import VueSocketIO from "vue-socket.io";
+import ConnectionStatus from "./components/ConnectionStatus.vue";
 
 console.log(VueSocketIO);
 
 export const Socket = SocketIO("http://localhost:3000", {
   transports: ["websocket"]
 });
+
+Vue.component("connection-status", ConnectionStatus);
 
 Vue.use(
   new VueSocketIO({
